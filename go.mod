aa1c79935820d5b331d467325ca8c897796c0624
hazem-the-dev <hazemgamaltaha3@gmail.com>
2025-04-26 15:10:19 +0300

    Getting local IP & subnet

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module wifi-monitor
 go 1.23.0
 
 require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
 	github.com/google/gopacket v1.1.19 // indirect
-	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 )
--- main.go
+++ main.go
@@ -3,11 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
+
+type Stats struct {
+	sync.Mutex
+	Packets int
+	Bytes   int
+	TCP     int
+	UDP     int
+	ICMP    int
+	Others  int
+}
 
 func main() {
 	// List all devices
@@ -29,12 +44,64 @@ func main() {
 	}
 	defer handle.Close()
 
-	pocketSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
-	fmt.Println("Listening on: ", device)
+	stats := &Stats{}
 
-	for packet := range pocketSource.Packets() {
-		fmt.Println("Packet captured at", packet.Metadata().Timestamp.Format(time.RFC3339))
-		fmt.Println(packet)
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			stats.Lock()
+			clearTerminal()
+			fmt.Println("Traffic Stats (1s refresh)")
+			fmt.Println("-______________________________________")
+			fmt.Printf("Total packets: %d\n", stats.Packets)
+			fmt.Printf("Total Bytes  : %d\n", stats.Bytes)
+			fmt.Printf("TCP          : %d\n", stats.TCP)
+			fmt.Printf("UDP          : %d\n", stats.UDP)
+			fmt.Printf("ICMP         : %d\n", stats.ICMP)
+			fmt.Printf("Others        : %d\n", stats.Others)
+			fmt.Println("_______________________________________")
+			stats.Unlock()
+		}
+	}()
+
+	for {
+		select {
+		case packet := <-packetSource.Packets():
+			stats.Lock()
+			stats.Packets++
+			stats.Bytes += len(packet.Data())
+
+			if netLayer := packet.NetworkLayer(); netLayer != nil {
+				if transportLayer := packet.TransportLayer(); transportLayer != nil {
+					switch transportLayer.LayerType() {
+					case layers.LayerTypeTCP:
+						stats.TCP++
+					case layers.LayerTypeUDP:
+						stats.UDP++
+					default:
+						stats.Others++
+					}
+				} else if icmpLayer := packet.Layer(layers.LayerTypeICMPv4); icmpLayer != nil {
+					stats.ICMP++
+				} else {
+					stats.Others++
+				}
+			}
+			stats.Unlock()
+		case <-stop:
+			fmt.Println("Exiting...")
+			return
+		}
+
 	}
+
+}
+
+func clearTerminal() {
+	fmt.Print("\033[H\033[2J")
 }
